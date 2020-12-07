3cf57061346d2d6b1461638d4d52dfaf9e13c538
SofieTorch <sofie.torch@outlook.com>
2020-12-06 21:09:43 -0400

    basic_movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    GameObject gmobj = default;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(gmobj.tag == "Stars"){
+            transform.position += Vector3.left * Time.deltaTime * 0.2f;
+        }
+        else if(gmobj.tag == "Planets"){
+            transform.position += Vector3.left * Time.deltaTime * 0.4f;
+        }
+        else {
+            transform.position += Vector3.right * Time.deltaTime * 0.6f;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed = 5f;
+
+    Animator anim;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        anim.SetBool("GoingUp", false);
+        anim.SetBool("GoingDown", false);
+
+        float movementV = Input.GetAxis("Vertical");
+        transform.position += Vector3.up * movementV * speed * Time.deltaTime;
+        float movementH = Input.GetAxis("Horizontal");
+        if(movementH > 0.1f){
+            transform.position += Vector3.right * movementH * speed * Time.deltaTime;
+        }
+
+        if(movementV > 0.1f) {
+            anim.SetBool("GoingUp", true);
+            anim.SetBool("GoingDown", false);
+        }
+        if(movementV < -0.1f) {
+            anim.SetBool("GoingUp", false);
+            anim.SetBool("GoingDown", true);
+        }
+    }
+}
